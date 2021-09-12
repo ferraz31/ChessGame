f01f5ddc3c2a5e16ffe25fdaa2366f484283b211
ferraz31 <jf.ferraz31@gmail.com>
2021-09-12 23:08:21 +0100

    Added ChessException; Transform matrix coordinates into chess coordinates;

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -27,12 +27,18 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+
+    }
+
     private void initialSetup() {
 
         try {
-            board.placePiece(new Rook(board, Color.WHITE), new Position(7, 1));
-            board.placePiece(new Rook(board, Color.WHITE), new Position(4, 1));
-            board.placePiece(new King(board, Color.BLACK), new Position(2, 3));
+            placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+            placeNewPiece('e', 1, new King(board, Color.WHITE));
+            placeNewPiece('e', 9, new King(board, Color.BLACK));
+
 
         } catch (BoardException e) {
             System.out.println(e.getMessage());
---
+++ src/main/java/chess/ChessPosition.java
@@ -0,0 +1,45 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+
+    private char column;
+    private Integer row;
+
+    public ChessPosition() {
+
+    }
+
+    public ChessPosition(char column, Integer row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8 ) {
+            throw new ChessException("Invalid position!");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    protected Position toPosition() {
+        return new Position(8-row, column - 'a');
+
+    }
+
+    protected static ChessPosition fromPosition(Position position) {
+        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+
+    public Integer getRow() {
+        return row;
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
+}
