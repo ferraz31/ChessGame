c5537c8c1183123b06cc9e851ba70eb7eae9ed31
ferraz31 <jf.ferraz31@gmail.com>
2021-09-12 23:58:29 +0100

    Moving pieces and capturing

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -4,14 +4,32 @@ import boardgame.Board;
 import boardgame.BoardException;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         try {
             ChessMatch chessMatch = new ChessMatch();
-            UI.printBoard(chessMatch.getPieces());
+
+            while (true) {
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
 
         } catch (BoardException e) {
             System.out.println(e.getMessage());
